2f31aaf9d0e5058387a28fa8c95784ecd6f510da
Chen-Han-NP <s10202961@connect.np.edu.sg>
2021-11-18 16:14:51 +0800

    get dats

--- app/src/main/java/com/example/pfdhelpinghand/CaregiverMainActivity.java
+++ app/src/main/java/com/example/pfdhelpinghand/CaregiverMainActivity.java
@@ -7,25 +7,60 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.time.Instant;
 
 public class CaregiverMainActivity extends AppCompatActivity {
+    FirebaseAuth fAuth;
+    TextView welcomeBanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caregiver_main);
+
+        FirebaseUser user = fAuth.getInstance().getCurrentUser();
+        String userID = user.getUid();
+
+        welcomeBanner = findViewById(R.id.welcomeBanner);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("Caregiver").document(userID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+
+                        welcomeBanner.setText((CharSequence) document.getData());
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
     }
 
 
@@ -35,4 +70,6 @@ public class CaregiverMainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
     }
+
+
 }
