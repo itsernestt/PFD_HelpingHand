ac5b46d8175a2059bb8960e12c896f17d4ccd624
nhaaki <S10206177@connect.np.edu.sg>
2022-01-11 17:21:22 +0800

    Modify Medication display greatly - refactoring, reformatting and using RecyclerView.

--- app/src/main/java/com/example/pfdhelpinghand/WeeklyMedicationActivity.java
+++ app/src/main/java/com/example/pfdhelpinghand/WeeklyMedicationActivity.java
@@ -1,8 +1,6 @@
 package com.example.pfdhelpinghand;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -111,7 +108,7 @@ public class WeeklyMedicationActivity extends AppCompatActivity {
 
         weeklyMedBackButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent navigateToPreviousPage = new Intent(WeeklyMedicationActivity.this, com.example.pfdhelpinghand.MedicationAlarmActivity.class);
+                Intent navigateToPreviousPage = new Intent(WeeklyMedicationActivity.this, MedicationAppointmentActivity.class);
                 startActivity(navigateToPreviousPage);
             }
         });
