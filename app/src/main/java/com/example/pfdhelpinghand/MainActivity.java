7c72a57070aff48b6b240a905d1e84613bc5e503
s10194187 <74453144+s10194187@users.noreply.github.com>
2022-01-21 22:21:24 +0800

    Added location checking function for elderly page

--- app/src/main/java/com/example/pfdhelpinghand/MainActivity.java
+++ app/src/main/java/com/example/pfdhelpinghand/MainActivity.java
@@ -3,9 +3,21 @@ package com.example.pfdhelpinghand;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.PorterDuff;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -15,7 +27,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,6 +40,10 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Document;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
@@ -59,26 +74,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button settingsButton = findViewById(R.id.settingsBtn);
-        settingsButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent navigateToSettings = new Intent(MainActivity.this, ElderlySettingsActivity.class);
                 startActivity(navigateToSettings);
             }
         });
 
         Button lostButton = findViewById(R.id.lostButton);
-        lostButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent navigateToLostPage = new Intent(MainActivity.this,HelpLostActivity.class);
+        lostButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent navigateToLostPage = new Intent(MainActivity.this, HelpLostActivity.class);
                 startActivity(navigateToLostPage);
             }
 
         });
 
         Button medAlarmButton = findViewById(R.id.medAlarmButton);
-        medAlarmButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent navigateToMedAlarmPage = new Intent(MainActivity.this,MedicationAlarmActivity.class);
+        medAlarmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent navigateToMedAlarmPage = new Intent(MainActivity.this, MedicationAlarmActivity.class);
                 startActivity(navigateToMedAlarmPage);
             }
 
@@ -86,69 +101,70 @@ public class MainActivity extends AppCompatActivity {
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                switch (v.getId()){
+                switch (v.getId()) {
                     case R.id.cancelButton:
                         Toast.makeText(getApplicationContext(), "Restarting to cancel SOS functions", Toast.LENGTH_SHORT).show();
                         MagicAppRestart.doRestart(MainActivity.this);
                 }
-            }});
-
+            }
+        });
 
 
         Button sosButton = findViewById(R.id.sosButton);
         TextView sosText = findViewById(R.id.sosText);
         sosButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                sosButton.setClickable(false);
-                new CountDownTimer(9000, 1000) {
+                                         public void onClick(View view) {
+                                             sosButton.setClickable(false);
+                                             new CountDownTimer(9000, 1000) {
 
-                    public void onTick(long millisUntilFinished) {
-                        sosText.setText("   " + millisUntilFinished / 1000);
-                    }
+                                                 public void onTick(long millisUntilFinished) {
+                                                     sosText.setText("   " + millisUntilFinished / 1000);
+                                                 }
 
-                    public void onClick(View view){
-                        cancel();
-                    }
+                                                 public void onClick(View view) {
+                                                     cancel();
+                                                 }
 
-                    public void onFinish() {
-                        sosText.setText("SOS");
-                        sosButton.setClickable(true);
-                        Intent callIntent = new Intent(Intent.ACTION_CALL);
-                        //startActivity(callIntent);
-                        Toast.makeText(getApplicationContext(), "Calling person, please wait", Toast.LENGTH_SHORT).show();
+                                                 public void onFinish() {
+                                                     sosText.setText("SOS");
+                                                     sosButton.setClickable(true);
+                                                     Intent callIntent = new Intent(Intent.ACTION_CALL);
+                                                     //startActivity(callIntent);
+                                                     Toast.makeText(getApplicationContext(), "Calling person, please wait", Toast.LENGTH_SHORT).show();
 
-                    }
+                                                 }
 
-                }.start();
+                                             }.start();
 
 
-                switch (view.getId()){
-                    case R.id.sosButton:
-                        Toast.makeText(getApplicationContext(), "SOS Button activated", Toast.LENGTH_SHORT).show();
+                                             switch (view.getId()) {
+                                                 case R.id.sosButton:
+                                                     Toast.makeText(getApplicationContext(), "SOS Button activated", Toast.LENGTH_SHORT).show();
 
-                }
-            }
-            public void buttonEffect(View button){
-                button.setOnTouchListener(new View.OnTouchListener() {
+                                             }
+                                         }
 
-                    public boolean onTouch(View v, MotionEvent event) {
-                        switch (event.getAction()) {
-                            case MotionEvent.ACTION_DOWN: {
-                                v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
-                                v.invalidate();
-                                break;
-                            }
-                            case MotionEvent.ACTION_UP: {
-                                v.getBackground().clearColorFilter();
-                                v.invalidate();
-                                break;
-                            }
-                        }
-                        return false;
-                    }
-                });
-            }
-        }
+                                         public void buttonEffect(View button) {
+                                             button.setOnTouchListener(new View.OnTouchListener() {
+
+                                                 public boolean onTouch(View v, MotionEvent event) {
+                                                     switch (event.getAction()) {
+                                                         case MotionEvent.ACTION_DOWN: {
+                                                             v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                                                             v.invalidate();
+                                                             break;
+                                                         }
+                                                         case MotionEvent.ACTION_UP: {
+                                                             v.getBackground().clearColorFilter();
+                                                             v.invalidate();
+                                                             break;
+                                                         }
+                                                     }
+                                                     return false;
+                                                 }
+                                             });
+                                         }
+                                     }
 
         );
 
@@ -173,17 +189,57 @@ public class MainActivity extends AppCompatActivity {
 //        })
 
         Button locationButton = findViewById(R.id.locationButton);
-        locationButton.setOnClickListener(new View.OnClickListener()
-        {
+        locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Uri gmmIntentUri =
-                        Uri.parse("geo:0,0?q=535 Clementi R, Singapore 599489");
+              /*  if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                   return;
+                }
+                LocationManager locationManager = (LocationManager)
+                        getSystemService(Context.LOCATION_SERVICE);
 
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
+                Criteria criteria = new Criteria();
+                criteria.setPowerRequirement(Criteria.POWER_LOW);
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                criteria.setSpeedRequired(true);
+                criteria.setAltitudeRequired(false);
+                criteria.setBearingRequired(false);
+                criteria.setCostAllowed(false);
+                String provider = locationManager.getBestProvider(criteria, true);
+
+                Location location = locationManager.getLastKnownLocation(provider);
+                String longitude = "Longitude: " + location.getLongitude();
+                Log.v("longitude", longitude);
+                String latitude = "Latitude: " + location.getLatitude();
+                Log.v("latitude", latitude); */
+
+                LocationManager locationManager = (LocationManager)
+                        getSystemService(Context.LOCATION_SERVICE);
+
+                LocationListener locationListener = new MyLocationListener();
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    ActivityCompat.requestPermissions((Activity) v.getContext(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+
+
+                    return;
+                } else {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+                }
+
+
+
 
 //
             }
@@ -195,9 +251,61 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
+
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
+    }
+
+    private class MyLocationListener implements LocationListener {
+
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+
+            String longitude = "Longitude: " + location.getLongitude();
+            Log.v("longitude", longitude);
+            String latitude = "Latitude: " + location.getLatitude();
+            Log.v("latitude", latitude);
+
+            //String uri = "geo:"+location.getLatitude()+","+location.getLongitude()+"?z=14.75";
+            String uri = "geo:0,0?q=Singapore 669614"; //placeholder
+            Log.v("uri", uri);
+            Uri gmmIntentUri =
+                    Uri.parse(uri);
+
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+        }
+
+        @Override
+        public void onLocationChanged(@NonNull List<Location> locations) {
+
+        }
+
+        @Override
+        public void onFlushComplete(int requestCode) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
     }
 }
