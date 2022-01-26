f6e339541cc8af406ff63adbe992125ae6c7dad8
Chen Han <s10202961@connect.np.edu.sg>
2022-01-26 21:18:24 +0800

    caretaker: fixed logout bug

--- app/src/main/java/com/example/pfdhelpinghand/CaregiverSetting.java
+++ app/src/main/java/com/example/pfdhelpinghand/CaregiverSetting.java
@@ -1,59 +1,32 @@
 package com.example.pfdhelpinghand;
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.nfc.Tag;
-import android.os.Bundle;
-
-import android.os.Handler;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class CaregiverSetting extends AppCompatActivity {
 
-    Button backBut;
-    FirebaseAuth fAuth;
-    FirebaseFirestore fStore;
-    FirebaseUser user;
+    Button backBut, logoutBut;
 
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caregiver_setting);
-        fAuth = FirebaseAuth.getInstance();
+
         fStore = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
         user = fAuth.getCurrentUser();
 
 
@@ -70,12 +43,27 @@ public class CaregiverSetting extends AppCompatActivity {
 
 
 
+        logoutBut = findViewById(R.id.caregiver_logout);
+        logoutBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                fAuth.signOut();
+                Intent intent = new Intent(CaregiverSetting.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+
+
+
+
+
     }
 
-    public void logout(View view) {
-        fAuth.signOut();
-        //startActivity(new Intent(CaregiverSetting.this, LoginActivity.class));
-        finish();
-    }
+
 
 }
